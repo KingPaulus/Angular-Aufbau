a87ba7b47ea16f0ed2b5b5522ebd0f91f48cf02d
King Paulus <mergetp02@gmail.com>
2025-04-28 14:34:51 +0200

    Router

--- crm/src/app/app.routes.ts
+++ crm/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { StartPageComponent } from './features/start-page/start-page.component';
+import { AboutPageComponent } from './features/about-page/about-page.component';
+import { NotFoundPageComponent } from './features/not-found-page/not-found-page.component';
+import { routesCustomers } from './features/customer/customers.routing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    ...routesCustomers,
+    {
+        path: '',
+        redirectTo: '/start',
+        pathMatch: 'full'
+    },
+    {
+        path: 'start',
+        component: StartPageComponent
+    },
+    {
+        path: 'about',
+        component: AboutPageComponent
+    },
+    {
+        path: '**',
+        component: NotFoundPageComponent
+    }
+];
---
+++ crm/src/app/features/customer/customers.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { CustomerComponent } from "./customer.component";
+
+export const routesCustomers: Routes = [
+    {
+        path: 'dashboard',
+        component: CustomerComponent
+    }
+]
