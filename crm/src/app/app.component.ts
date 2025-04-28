b89d2ac895dadef61be0dae02660dcd1e1d3d6b3
King Paulus <mergetp02@gmail.com>
2025-04-28 13:29:36 +0200

    Navigation Hinzufügen

--- crm/src/app/app.component.ts
+++ crm/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+  <div class="container mt-4">
+    <router-outlet></router-outlet>
+  </div>
+  `
 })
 export class AppComponent {
   title = 'CRM v0.1';
