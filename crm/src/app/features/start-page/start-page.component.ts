b18c70ee624bd88d6f0268cda07b7ac8f257827b
King Paulus <mergetp02@gmail.com>
2025-05-02 11:13:58 +0200

    Front Page

--- crm/src/app/features/start-page/start-page.component.ts
+++ crm/src/app/features/start-page/start-page.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-start-page',
-  imports: [],
+  imports: [
+    JsonPipe
+  ],
   templateUrl: './start-page.component.html',
   styleUrl: './start-page.component.scss'
 })
-export class StartPageComponent {
+export class StartPageComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.loadNews();
+  }
+
+  news = signal<number[]>([]);
+
+  loadNews() {
+    of([3,4,56]).subscribe((n) => {
+
+      this.news.set(n);
+    })
+  }
 
 }
